93d8892b8beb5143225a5861af562969635bcc39
johnny <jbui923@gmail.com>
2021-04-12 10:52:53 -0700

    starter

--- README.md
+++
@@ -1,1 +0,0 @@
-# practice-creating-handling-promises
---
+++ starter/part-2.js
@@ -0,0 +1,35 @@
+function stretch(timeLeft) {
+
+}
+
+function runOnTreadmill(timeLeft) {
+
+}
+
+function liftWeights(timeLeft) {
+
+}
+
+function workout(totalTime) {
+	
+}
+
+// TESTS
+
+// comment in each invocation of your workout function below and run
+// the file (node part-2.js) to see if you get the expected output
+
+// workout(1000);
+// 		done stretching
+// 		Error:  you dont have enough time to run on treadmill
+
+// workout(2000);
+// 		done stretching
+// 		done running on treadmill
+// 		Error:  you dont have enough time to lift weights
+
+// workout(4000);
+// 		done stretching
+// 		done running on treadmill
+// 		done lifting weights
+// 		done workout out
